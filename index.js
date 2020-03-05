5ea113e3b6ac65230089eba346043b62d2c98735
Mauricio Cantu <ciciocantuu@gmail.com>
2020-03-04 23:08:17 -0300

    feat: added express

--- index.js
+++ index.js
@@ -6,5 +6,18 @@
  */
 
 const initBot = require('./initBot')
+const express = require('express')
+const app = express()
+
+app.get('*', (req, res) => {
+  res.set('Content-Type', 'text/html')
+  res.send(
+    new Buffer(
+      "<div style='padding: 30px;max-width: 500px; font-family: monospace;'>Hey there! This is the server for <b><a style='font-size: 20px;' href='http://twitter.com/ThisDayBot' target='blank'>@ThisDayBot</a></b> Twitter bot. Follow it to get to know historical facts that happened on the same current day but in other years.<br>Built by Mauricio Cantu, 2020.<br>GitHub: <a href='http://www.github.com/mauricio-cantu'> github.com/mauricio-cantu</a></div>"
+    )
+  )
+})
+
+app.listen(3000)
 
 initBot()
