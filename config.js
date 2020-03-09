b0e958a1fa6e92064531718cb81a160ee3e521bb
Mauricio Cantu <ciciocantuu@gmail.com>
2020-03-09 03:14:13 -0300

    feat: added webhook to listen to events of interaction with account

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apiConfig: {
+    consumer_key: 'UhcbaUimj1tYhjaHXVkSOmpbM',
+    consumer_secret: 'axQwvN0kin5ntnNutMMn6kvOuasGN8nd0zDyxIZ2rNUqghAau9',
+    access_token: '1234845543895052288-gycFMBoC08Bab4CGdm2mofAMM1OM1U',
+    access_token_secret: '4YuCVOYbGoYSIpaaIDWML0xlUxslHRPcix55IfZaE677T'
+  },
+  constants: {
+    HASHTAG: '#OnThisDay',
+    MAX_CHARACTERS: 275,
+    INTERVAL_TWEETS: 20 * 60000,
+    PING_INTERVAL: 28 * 60000
+  }
+}
