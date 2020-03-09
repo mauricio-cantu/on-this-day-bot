e24d91c94d4d71d05606819a11fc6d5a9244e695
Mauricio Cantu <ciciocantuu@gmail.com>
2020-03-09 14:36:01 -0300

    testing heroku env vars

--- bots/twitterBot.js
+++ bots/twitterBot.js
@@ -4,10 +4,10 @@ module.exports = twitterBot
 const Twit = require('twit')
 const initBot = require('./initBot')
 const apiConfig = {
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token: process.env.ACCESS_TOKEN,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 }
 const {
   constants: { HASHTAG, MAX_CHARACTERS, INTERVAL_TWEETS }
