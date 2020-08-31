80e51deb4f24270d740e677ddd5aad077730d0de
Mauricio Cantu <ciciocantuu@gmail.com>
2020-08-31 11:58:05 -0300

    test wiki api

--- src/bots/wikiBot.js
+++ src/bots/wikiBot.js
@@ -1,42 +1,45 @@
-const wiki = require('wikijs').default
+const wiki = require("wikijs").default;
 
 const wikiBot = {
   contents: {
     events: {},
     births: {},
-    deaths: {}
+    deaths: {},
   },
 
-  init: async function(query) {
+  init: async function (query) {
     return (
       wiki()
         // search for page of current day
         .search(query, 1)
-        .then(data =>
+        .then((data) =>
           wiki()
             // takes first result (the most corresponding)
             .page(data.results[0])
-            .then(page =>
+            .then((page) =>
               // categorize contents in deaths, births and events
-              page.sections().then(sections => {
-                wikiBot.categorize(sections)
+              page.sections().then((sections) => {
+                wikiBot.categorize(sections);
               })
             )
         )
-    )
+        .catch((err) => {
+          console.log("wikiBot - init", err);
+        })
+    );
   },
 
-  categorize: function(sections) {
+  categorize: function (sections) {
     for (section of sections) {
-      if (section.title == 'Events') {
-        this.contents.events = section.content
-      } else if (section.title == 'Births') {
-        this.contents.births = section.content
-      } else if (section.title == 'Deaths') {
-        this.contents.deaths = section.content
+      if (section.title == "Events") {
+        this.contents.events = section.content;
+      } else if (section.title == "Births") {
+        this.contents.births = section.content;
+      } else if (section.title == "Deaths") {
+        this.contents.deaths = section.content;
       }
     }
-  }
-}
+  },
+};
 
-module.exports = wikiBot
+module.exports = wikiBot;
